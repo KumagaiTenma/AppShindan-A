d7f2b2a8aeff6b5321f9e6f603b0e84f791ee305
Your Name <tenma.kumagai.sys@gmail.com>
2017-06-27 10:00:09 +0900

    問４の実装

--- src/Shindan.java
+++ src/Shindan.java
@@ -2,20 +2,20 @@
 public class Shindan {
 
 	public static void main(String[] args) {
-		//メイン処理
+		//繝｡繧､繝ｳ蜃ｦ逅�
 		int situ1 = situmon1();
-		//回答内容で診断結果を考える処理
+		//蝗樒ｭ泌��螳ｹ縺ｧ險ｺ譁ｭ邨先棡繧定��縺医ｋ蜃ｦ逅�
 
-		//回答結果を出力
+		//蝗樒ｭ皮ｵ先棡繧貞�ｺ蜉�
 
 
 	}
 
 	public static int situmon1() {
-		//質問内容を表示
-		//回答内容を表示(1：男性　2：女性)
-		//ユーザーが答えを入力
-		//返り値ありでもおｋ
+		//雉ｪ蝠丞��螳ｹ繧定｡ｨ遉ｺ
+		//蝗樒ｭ泌��螳ｹ繧定｡ｨ遉ｺ(1�ｼ夂塙諤ｧ縲�2�ｼ壼･ｳ諤ｧ)
+		//繝ｦ繝ｼ繧ｶ繝ｼ縺檎ｭ斐∴繧貞�･蜉�
+		//霑斐ｊ蛟､縺ゅｊ縺ｧ繧ゅ♀�ｽ�
 
 		int a = 0;
 		return a;
@@ -30,10 +30,23 @@ public class Shindan {
 
 	}
 
-	public void sirumon4() {
+	public int sirumon4() {
+		int i;
+		//質問
+		System.out.println("質問4");
+		System.out.println("好きな花は？");
+		System.out.println("1:薔薇  2:百合");
 
+		//解答判定(1と2以外選んだらループ)
+		do{
+			System.out.println("解答: " + i = sc.nextInt());
+			if( i < 1 || 2 < i) {
+				System.out.println("指定した値を入力してください");
+				System.out.println();
+			}
+	}while( i < 1 || 2 < i);
+		return i;
 	}
-
 	public void sirumon5() {
 
 	}
